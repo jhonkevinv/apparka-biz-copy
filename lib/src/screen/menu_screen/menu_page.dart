7303253390ba61d78d68e59af31beccfa657dc53
jhonkevinv <jhonkevinvl22@gmail.com>
2025-02-13 13:45:52 -0500

    update

--- lib/src/screen/menu_screen/menu_page.dart
+++ lib/src/screen/menu_screen/menu_page.dart
@@ -1,6 +1,5 @@
 import 'package:estacionamiento_aparka/src/screen/login_screen/login_page.dart';
 import 'package:estacionamiento_aparka/src/screen/report_screen/report_page.dart';
-import 'package:estacionamiento_aparka/src/screen/validation_screen/selection_page.dart';
 import 'package:estacionamiento_aparka/src/screen/validation_screen/validation_page.dart';
 import 'package:estacionamiento_aparka/src/theme/app_theme.dart';
 import 'package:flutter/material.dart';
@@ -53,28 +52,7 @@ class _MenuPageState extends State<MenuPage> {
                   Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) => const SelectionPage(mapData: {
-                            "numero": "string",
-                            "placa": "string",
-                            "code": "string",
-                            "horaIngreso": "string",
-                            "horaSalida": "string",
-                            "tiempoTotal": "string",
-                            "idMovimiento": 0,
-                            "idPlaya": 0,
-                            "descripcionPlaya": "string",
-                            "tiempoMinutos": 0,
-                            "systemStatus": {
-                              "fechaHora": "2025-02-03T14:27:12.289Z"
-                            },
-                            "tarifas": [
-                              {
-                                "id": "123",
-                                "descripcion": "string"
-                              }
-                            ],
-                            "vales": []
-                          },)));
+                          builder: (context) => const ValidationPage()));
                 },
                 child: Column(
                   children: [
